bd1b6aab6ccd4c38ae75fb0fe85b45fa9cdd0cfc
Revo <berkem_elgul@hotmail.com>
2023-04-27 20:35:06 +0300

    implemented heuristics algorithm

--- binpacking_algorithms.py
+++ binpacking_algorithms.py
@@ -1,7 +1,10 @@
 import random
+import copy
+import sys
 
 #brute force algorithm
-def brute_force_algorithm(B,n,U):
+def brute_force_algorithm(B,U_initial):
+    U = U_initial.copy()
     k = [] #initializing the bin array
     while(len(U) != 0):
         k.append(B-U[0])
@@ -19,10 +22,24 @@ def brute_force_algorithm(B,n,U):
             else:
                 U.pop(best_index)
                 k[-1] = best_gap
-    print("RUN NUMBER: ", x+1, " THE RESULT IS AS FOLLOWING:")
-    print(n," objects placed in ", len(k), " bins, each bin with capacity of ", B,"\n")
+    return (len(k))
 
-#def heuristic_algorithm():
+def heuristic_algorithm(B,U_initial):
+    U = U_initial.copy()
+    k = []
+    k.append(B)
+    while(len(U) != 0):
+        could_not_bin = True
+        for i in range(len(k)):
+            if(k[i] >= U[0]):
+                k[i] = k[i] - U[0]
+                U.pop(0)
+                could_not_bin = False
+                break
+        if (could_not_bin):
+            k.append(B-U[0])
+            U.pop(0)
+    return (len(k))
 
 #random input generator
 def random_input_generator(n,B):
@@ -32,10 +49,18 @@ def random_input_generator(n,B):
     return U
 
 #implementation of brute force algorithm
-B = random.randint(50,100)
-n = random.randint(50,100)
-U = random_input_generator(n,B)
+
+sys.stdout = open('output.txt', 'w')
 run_number = 20
 for x in range(run_number):
-    brute_force_algorithm(B,n,U)
-    #heuristic_algorithm(B,n,U)
+    B = random.randint(50,100)
+    n = random.randint(50,100)
+    U_initial = random_input_generator(n,B)
+    b_result_bin = brute_force_algorithm(B,U_initial)
+    h_result_bin = heuristic_algorithm(B,U_initial)
+    print("RUN NUMBER:", x+1, "THE RESULT IS AS FOLLOWING:")
+    print("Given",n, "items in U and bins with capacity B of", B)
+    print("Brute force algorithm managed to pack input into", b_result_bin ,"bins." )
+    print("Heuristic algorithm managed to pack input into", h_result_bin ,"bins." )
+    print("\n")
+sys.stdout.close()
